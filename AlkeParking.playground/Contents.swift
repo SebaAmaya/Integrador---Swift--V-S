2c1511d6ccfcf072deda8aa7c94b3243f759680d
Vanesa Mizrahi <vanesa.mizrahi@mercadolibre.com>
2021-09-13 12:06:29 -0300

    Implemented Func in  Parking

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -11,6 +11,66 @@ protocol Parkable {
 
 struct Parking {
     var vehicles: Set<Vehicle> = []
+    var checkAndEarnings: (checkedouts: Int, earnings: Int) = (0,0)
+    let maxCapacity = 20
+    
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
+        
+        guard maxCapacity > vehicles.count else {
+            print("Sorry, the check-in failed. The Parking is full")
+            onFinish(false)
+            return
+        }
+        
+        if vehicles.contains(vehicle){
+            print("Sorry, the check-in failed. The plate: \(vehicle.plate) has already exist")
+            onFinish(false)
+            return
+        } else {
+            vehicles.insert(vehicle)
+            print("Welcome to AlkeParking!")
+            onFinish(true)
+            return
+        }
+    }
+    
+    mutating func checkOutVehicle(_ plate: String, onSuccess: (Int) -> Void , onError: (Bool)-> Void ){
+        
+        var vehicleToCheckOut: Vehicle
+        
+        for vehicle in vehicles {
+            if vehicle.plate == plate {
+                vehicleToCheckOut = vehicle
+                let totalFee = calculateFee(vehicleToCheckOut.type, parkedTime: vehicleToCheckOut.parkedTime, hasDiscountCard: vehicleToCheckOut.discountCard != nil)
+                vehicles.remove(vehicleToCheckOut)
+                checkAndEarnings.checkedouts += 1
+                checkAndEarnings.earnings += totalFee
+                onSuccess(totalFee)
+                return
+            }
+        }
+        
+        onError(true)
+        return
+
+    }
+    func calculateFee(_ vehicle: VehicleType, parkedTime: Int, hasDiscountCard: Bool) -> Int {
+        
+        var feeRate = vehicle.timeRate()
+        
+        if parkedTime > 120 {
+            let additional = Int(ceil(((Double(parkedTime) - 120) / 15 ) * 5 ))
+            feeRate += additional
+        }
+        
+        if hasDiscountCard {
+            return Int(Double(feeRate) * 0.85)
+        } else {
+            return feeRate
+        }
+    }
+
+    
 }
 
 enum VehicleType {
@@ -55,3 +115,4 @@ struct Vehicle: Parkable, Hashable {
     }
     
 }
+
